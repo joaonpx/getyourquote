f79b970f07359d896ca0eb6cba0f054d40276497
Npx <cheyprivado@gmail.com>
2024-03-18 03:00:09 -0300

    🎉 Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <div className="h-screen font-body bg-stone-950 text-stone-100 flex items-center">
+      <main>
+        <p>
+          &quot;Believe in yourself! Have faith in your abilities! Without a
+          humble but reasonable confidence in your own powers you cannot be
+          successful or happy.&quot;
+        </p>
+        <h1 className="text-right">Norman Vincent Peale</h1>
+      </main>
+    </div>
+  );
+}
